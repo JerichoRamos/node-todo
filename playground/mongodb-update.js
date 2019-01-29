e41362c1ef1be66d2ba7f893377779ef5fa7452d
Jericho Ramos <jericho.ramos@jway.com>
2019-01-29 15:01:47 +0800

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,44 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect("mongodb://localhost:27017//TodoApp",{ useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // Update doc content
+
+    // TODOs
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5c4fcfadb04c2689c319be86')
+    //     }, {
+    //         $set: {                     // set new data
+    //             text: 'Dahyun'                            
+    //         }
+    //     }, {
+    //         returnOriginal: 'Sana'      // original data
+    //     }).then((result) => {
+    //         console.log(result);
+    //     });
+
+    // USERs
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5c4fa5b24de36e2d28f57cd8')
+        }, {
+            $set: {                     // set new data
+                name: 'Dahyun'                            
+            },
+            $inc: {
+                age: -1
+            }
+        }, {
+            returnOriginal: 'Ashe'      // original data
+        }).then((result) => {
+            console.log(result);
+        });
+
+
+    // client.close();
+});
