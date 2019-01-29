cea08bf3ae050c4273c2210f55b744c1d0914315
Jericho Ramos <jericho.ramos@jway.com>
2019-01-29 13:00:33 +0800

    Add delete script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -5,7 +5,7 @@ MongoClient.connect("mongodb://localhost:27017//TodoApp",{ useNewUrlParser: true
     if (err) {
         return console.log('Unable to connect to MongoDB server');
     }
-    console.log('Connected to MongoDB server');
+    // console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
     // For TODOs
