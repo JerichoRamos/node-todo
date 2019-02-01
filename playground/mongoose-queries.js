fed53dba13595e5b6411e1ee4a4e24e0c66ccc2a
Jericho Ramos <jericho.ramos@jway.com>
2019-02-01 11:20:40 +0800

    Add queries playground file

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,25 +1,50 @@
 const {ObjectID} = require('mongodb');
-
 const {mongoose} = require('./../server/db/mongoose');
+
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
-var id = '5c5290a411f77c1ef4dc632b1';
+// input (TODOs)
+// var idTodo = '5c53aeeaea6dc82a84a1ee8c1';
 
-Todo.find({ 
-    _id: id
-  }).then((todos) => {
-    console.log('Todos', todos);
-  }).catch((e) => console.log('ERROR'));
-  
-  Todo.findOne({
-    _id: id
-  }).then((todo) => {
-    console.log('Todo', todo);
-  }).catch((e) => console.log('ERROR'));
+// input (USERs)
+var idUser = '5c51063b389d9e26189f1d19';
 
-Todo.findById().then((todo) => {
-  if (!todo) {
-    return console.log('Id not found');
-  }
-    console.log('Todo By Id', todo);
-}).catch((e) => console.log('ERROR'));
+// if(!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// };
+
+
+// Find id - Method #1 (TODOs)
+// Todo.find({ 
+//     _id: idTodo
+//   }).then((todos) => {
+//     console.log('Todos', todos);
+//   });
+
+
+// Find id - Method #2 (TODOs)
+//   Todo.findOne({
+//     _id: idTodo
+//   }).then((todo) => {
+//     console.log('Todo', todo);
+//   });
+
+
+// Find id - Method #3 (TODOs)
+// Todo.findById(idTodo).then((todo) => {
+//     if(!todo) {
+//       return console.log('Id not found');
+//     }
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log('ERROR'));
+
+
+User.findById(idUser).then((user) => {
+    if (!user) {
+      return console.log('Unable to find user');
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log('ERROR');
+});
